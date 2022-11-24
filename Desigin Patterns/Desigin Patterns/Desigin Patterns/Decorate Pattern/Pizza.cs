a18786ff8231c1d4426ab74f40a33507ef6e7f10
AbNashaddhi <117175615+AbNashaddhi@users.noreply.github.com>
2022-11-24 15:11:08 +0530

    Add files via upload

---
+++ Desigin Patterns/Desigin Patterns/Desigin Patterns/Decorate Pattern/Pizza.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desigin_Patterns.Decorate_Pattern
+{
+    //Concreate Implemation
+    public class Pizza : IPizza
+    {
+        public string GetPizzaType()
+        {
+            return "Order To Pizza";
+        }
+    }
+
+    //Base Decorator
+    public class PizzaDecorator : IPizza
+    {
+        private IPizza _pizza;
+        public PizzaDecorator(IPizza pizza)
+        {
+            _pizza = pizza;
+        }
+        public virtual string GetPizzaType()
+        {
+            return _pizza.GetPizzaType();
+        }
+    }
+    /// <summary>
+    /// Concrete Decator 
+    /// Like Adding Extra items
+    /// </summary>
+    public class CheeseDecator:PizzaDecorator
+    {
+        public CheeseDecator(IPizza pizza):base(pizza)
+        {
+
+        }
+        public override string GetPizzaType()
+        {
+            string type = base.GetPizzaType();
+            type += "\r\n Extra Chesee____";
+            return type;
+
+        }
+
+    }
+    //Extra Onion with order
+    public class OnionDecator:PizzaDecorator
+    {
+        public OnionDecator(IPizza pizza):base(pizza)
+        {
+
+        }
+        public override string GetPizzaType()
+        {
+            string type = base.GetPizzaType();
+            type += "\r\n Extra Onion";
+            return type;
+        }
+    }
+    /// <summary>
+    
+    /// </summary>
+    public class TomotoDecator : PizzaDecorator
+    {
+        public TomotoDecator(IPizza pizza) : base(pizza)
+        {
+
+        }
+        public override string GetPizzaType()
+        {
+            string type = base.GetPizzaType();
+            type += "\r\n Extra Onion";
+            return type;
+        }
+    }
+}
---
+++ Desigin Patterns/Desigin Patterns/Desigin Patterns/Program.cs
@@ -0,0 +1,103 @@
+﻿using Desigin_Patterns.AdpatarPattern;
+using Desigin_Patterns.Decorate_Pattern;
+using Desigin_Patterns.Observer;
+using Desigin_Patterns.ObserverPattern;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Desigin_Patterns.FactoryPattern;
+
+namespace Desigin_Patterns
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            ////Decorate_Pattern 
+            ///Mix and match extra items
+            IPizza pizza = new Pizza();
+            IPizza cheese = new CheeseDecator(pizza);
+            IPizza onion = new OnionDecator(cheese);
+            IPizza tomoto = new TomotoDecator(onion);
+            Console.WriteLine(tomoto.GetPizzaType());
+            Console.ReadLine();
+
+            //Adapter
+            string[,] employeesArray = new string[5, 4]
+            {
+                {"101","Chinna","SE","25000"},
+                {"102","Rahul","SE","20000"},
+                {"103","Rakhi","SSE","15000"},
+                {"104","Pam","SE","48000"},
+                {"105","Sara","SSE","50000"}
+            };
+
+            ITarget target = new EmployeeAdapter();
+            Console.WriteLine("HR system passes employee string array to Adapter\n");
+            target.ProcessCompanySalary(employeesArray);
+            Console.WriteLine("End of Adapter");
+
+            //observer
+            Subject onePlus = new Subject("Red MI Mobile", 10000, "Out Of Stock");
+            //User Chinna will be created and user1 object will be registered to the subject
+            OBSERVER user1 = new OBSERVER("Chinna", onePlus);
+            OBSERVER user2 = new OBSERVER("Nashaddhi", onePlus);
+            OBSERVER user3 = new OBSERVER("Haseena", onePlus);
+
+            Console.WriteLine("One Puls  Mobile current state : " + onePlus.getAvailability());
+            Console.WriteLine();
+            // Now product is available
+            onePlus.setAvailability("Available");
+
+            Console.WriteLine("End of Observer");
+
+            //singleton
+            Singleton parent = Singleton.GetInstance;
+            parent.PrintDetails("From Parent");
+            Singleton child = Singleton.GetInstance;
+            child.PrintDetails("From Child");
+            Console.WriteLine("End of Singleton");
+
+            //factory
+            CreditCard creditCard = new PlatinumFactory().CreateProduct();
+            if (creditCard != null)
+            {
+                Console.WriteLine("Card Type : " + creditCard.GetCardType());
+                Console.WriteLine("Credit Limit : " + creditCard.GetCreditLimit());
+                Console.WriteLine("Annual Charge :" + creditCard.GetAnnualCharge());
+            }
+            else
+            {
+                Console.Write("Invalid Card Type");
+            }
+            Console.WriteLine("--------------");
+            creditCard = new MoneyBackFactory().CreateProduct();
+            if (creditCard != null)
+            {
+                Console.WriteLine("Card Type : " + creditCard.GetCardType());
+                Console.WriteLine("Credit Limit : " + creditCard.GetCreditLimit());
+                Console.WriteLine("Annual Charge :" + creditCard.GetAnnualCharge());
+            }
+            else
+            {
+                Console.Write("Invalid Card Type____");
+            }
+            creditCard = new TitaniumFactory().CreateProduct();
+            if (creditCard != null)
+            {
+                Console.WriteLine("CardType:" + creditCard.GetCardType());
+                Console.WriteLine("Credit Limit:" + creditCard.GetCreditLimit());
+                Console.WriteLine("Annual Charge:" + creditCard.GetAnnualCharge());
+            }
+            else
+            {
+                Console.Write("Invalid Card Type____");
+
+            }
+
+        }
+    }
+}
